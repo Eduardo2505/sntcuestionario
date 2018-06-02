ec32b3240a42939ccdfb9f02c8403dbec52106dc
Eduardo Padilla Cruz <eduardopadillacz@gmail.com>
2018-06-02 12:58:29 -0500

    produccion

--- application/config/database.php
+++ application/config/database.php
@@ -8,7 +8,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '35.226.65.43',
+	'hostname' => 'localhost',
 	'username' => 'root',
 	'password' => 'seiter',
 	'database' => 'sntcenter_db',
